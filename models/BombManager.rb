159de0423dbf12b6daf460a21883c9b483cda4f9
Danilo Pinotti <danilopinotti@hotmail.com>
2016-05-24 22:13:45 -0300

    first bomb manager prototype

--- models/BombManager.rb
+++ models/BombManager.rb
@@ -1,1 +1,29 @@
 # Encoding: UTF-8
+class BombManager
+	@planted_bombs
+	@planted_bomb_limit
+	@explosion_range
+
+	def initialize
+
+	end
+
+	def plantedBombs
+
+	end
+
+	def plantedBombsLimit
+
+	end
+
+	def plantBomb x, y
+
+	end
+
+	#range 
+	def range
+
+	end
+
+	def
+end
