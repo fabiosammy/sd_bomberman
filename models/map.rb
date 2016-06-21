1184c7f2d20b742a924f2a107c8886b4ca092a78
Jonas <jonaswdasilvaa@gmail.com>
2016-06-21 20:40:02 -0300

    classe mapa renderizando

--- models/map.rb
+++ models/map.rb
@@ -1,31 +1,56 @@
 # Encoding: UTF-8
-class Map
+require 'rubygems'
+require 'gosu'
+
+#WIDTH, HEIGHT = 1750, 900
+BLOCK_DIM = 64
+
+class Map < Gosu::Window
+	
+	#file = File.open("assets/images/cenarios/map1")
+	#background_image = Gosu::Image.new("assets/images/cenarios/map3.bmp", :tileable => true)
 	@file
-	
-	#background
-	
-	def initialize	
+	@width
+	@height
+
+	def initialize #file, background_image	
 		# pegar o arquivo, criar uma array de acordo com o arquivo	
-		@file = File.open("map2.txt")
+		@file = File.open("assets/images/cenarios/map1") #unless file
+		window = File.open("assets/images/cenarios/map1")
 		@locals = Hash.new
+		@block = Gosu::Image.new("assets/images/cenarios/bricks1.png")
+		@background_image = Gosu::Image.new("assets/images/cenarios/map0.bmp", :tileable => true) #unless background_image
+	
+		
+		set_window_size window
+		#metodo para identificar os caracteres do file
 		generateBlocks @file
-		canMoveTo
+		
+		super @width-BLOCK_DIM, @height
+    	#@client = Client.new(server, port)
+    	self.caption = "BomberMANO"
+    	
 	end
 
-	def render
-
+	def set_window_size file
+		
+		line = file.readlines
+		@width = line[0].size*BLOCK_DIM
+		@height = line.length*BLOCK_DIM
 	end
 
 	def generateBlocks file
-		#pega todo o arquivo
+		#adquire a quantidade de linhas de file(arquivo que foi lido)
 		line = file.readlines
+		linhas = line.length
+		
 		for j in (0..line.length- 1)
 			#pega apenas a linha 
 			each_line = line[j]
-			#printa a linha que está selecionada
-			#puts each_line
+			
 			#verifica cada caracter separadamente e atribui os blocos
-			for i in (0..line.length)
+			for i in (0..line[0].size- 1)
+				
 	  			character = each_line.slice(i, 1) 
 	  			case character
 	  			when 'm'
@@ -42,35 +67,42 @@ class Map
 	  			end
 			end
 		end
+		#puts @locals
 	end
 
-	def canMoveTo
-		for h in (0..@locals.length- 1)
-		 	for l in (0..@locals.length) 
-	   			case @locals[[h,l]]
-	   			when 'metal'
-	   				@locals[[h,l]] = "não anda"
-	   			when 'caminho'
-	   				@locals[[h,l]] = "anda"
-	   			when 'ese'
-	   				@locals[[h,l]] = "NASCE MANO"
-	   			else
-	   				
-	   			end
-		 	end
-	 	end
-	 p @locals
+	def update_blocks file_server
+
 	end
 
-	#retorna se o personagem pode ou nao se mover para o bloco X Y
-	def getBlockType (posX, posY)
-		# true: lugar vazio ou com bloco de buff
-		# false: demais ocasioes
+	def can_move_to posX, posY
+		
 	end
 
+	def block_now
 
+	end
 
+   	def draw
+  		
+     	#@background_image.draw(0, 0, 0)
+	     line = @locals.length
+
+		 h = 0
+	     k = 0
+		 for j in (0..line- 1)
+		 	j = j*BLOCK_DIM
+		 	for i in (0..line)
+		 	 	i = i*BLOCK_DIM
+		 	 	if(@locals[[h,k]] == "metal")
+		    			@block.draw(i,j,0)
+		    		end	
+		    		k += 1
+		  	end
+		  	h += 1
+		  	k = 0
+		  end
+   	end
 end
 
 
-map = Map.new
+Map.new.show if __FILE__== $0
